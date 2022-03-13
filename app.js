40951375b694c4b014085e738ca6c39708d132a3
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-13 21:29:57 +0600

    nav added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // nav button handle 
 let toggle = document.querySelector('.toggle');
 let nav = document.querySelector('.nav');
+let navBar = document.querySelector('#navBar');
+console.log(navBar);
 document.querySelector('.toggle-parent').addEventListener('click', () => {
     console.log('clck');
     nav.classList.toggle('open');
+    navBar.classList.toggle('navbar-show');
     toggle.classList.toggle('active');
+
 })
