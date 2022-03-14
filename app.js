44f3cd95091a380a0273e144b312496543b87a0a
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-14 18:56:55 +0600

    nav btn fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let emailBtn = document.getElementById("email-btn");
 let toggle = document.querySelector('.toggle');
 let nav = document.querySelector('.nav');
 let navBar = document.querySelector('#navBar');
-document.querySelector('.toggle-parent').addEventListener('click', () => {
+document.getElementById('nav-container').addEventListener('click', () => {
     nav.classList.toggle('open');
     navBar.classList.toggle('navbar-show');
     toggle.classList.toggle('active');
